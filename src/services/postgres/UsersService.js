14bb0b103a4ea66749fbf25619c6f4dbe3db6122
Regi Witanto <witantoregi@gmail.com>
2024-07-25 15:00:10 +0700

    create verifyNewUsername func in UsersService

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,7 +1,24 @@
 const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class UsersService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {}
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+    if (result.rows.length > 0) {
+      throw new InvariantError(
+        'Gagal menambahkan user. Username sudah digunakan.'
+      );
+    }
   }
 }

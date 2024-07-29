efffa503206eff1cdacccc9a042bc1d8a34f3903
Regi Witanto <witantoregi@gmail.com>
2024-07-29 10:23:25 +0700

    create routes post playlist

---
+++ src/api/playlists/routes.js
@@ -0,0 +1,9 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/playlists',
+    handler: (request, h) => handler.postPlaylistHandler(request, h),
+  },
+];
+
+module.exports = routes;

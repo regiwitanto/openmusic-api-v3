5c83d12e5113464d8ec809448d58e8b5d8ec1fdd
Regi Witanto <witantoregi@gmail.com>
2024-07-29 10:30:22 +0700

    create playlist handler

---
+++ src/api/playlists/handler.js
@@ -0,0 +1,25 @@
+class PlaylistsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+  }
+
+  async postPlaylistHandler(request, h) {
+    this._validator.validatePlaylistPayload(request.payload);
+    const { name } = request.payload;
+
+    const playlistId = await this._service.addPlaylist({
+      name,
+    });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        playlistId,
+      },
+    });
+    response.code(201);
+
+    return response;
+  }
+}

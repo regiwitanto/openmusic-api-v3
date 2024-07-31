8950a7dc471a72dfdab3d47375c4e69bb5223baf
Regi Witanto <witantoregi@gmail.com>
2024-07-31 10:08:06 +0700

    create collaborations routes, post collab

---
+++ src/api/collaborations/routes.js
@@ -0,0 +1,12 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/collaborations',
+    handler: (request, h) => handler.postCollaborationHandler(request, h),
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+];
+
+module.exports = routes;

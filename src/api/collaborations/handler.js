ed9224ad63c19b5f89028bf346b9fdfe223b4df7
Regi Witanto <witantoregi@gmail.com>
2024-07-31 10:30:59 +0700

    create delete collab handler

--- src/api/collaborations/handler.js
+++ src/api/collaborations/handler.js
@@ -31,6 +31,30 @@ class CollaborationsHandler {
       return error;
     }
   }
+
+  async deleteCollaborationHandler(request, h) {
+    try {
+      this._validator.validateDeleteCollaborationsPayload(request.payload);
+      const { playlistId, userId } = request.payload;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._playlistsService.verifyPlaylistOwner(
+        playlistId,
+        credentialId
+      );
+      await this._collaborationsService.deleteCollaboration(playlistId, userId);
+
+      const response = h.response({
+        status: 'success',
+        message: 'Kolaborasi berhasil dihapus',
+      });
+      response.code(200);
+
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
 module.exports = CollaborationsHandler;

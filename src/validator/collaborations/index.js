0c1779856a77e937740f69038e4c3e7f98bf7c10
Regi Witanto <witantoregi@gmail.com>
2024-07-31 10:17:40 +0700

    create post collab validator

---
+++ src/validator/collaborations/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { PostCOllaborationsPayloadSchema } = require('./schema');
+
+const CollaborationsValidator = {
+  validatePostCollaborationsPayload: (payload) => {
+    const validationResult = PostCOllaborationsPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = CollaborationsValidator;

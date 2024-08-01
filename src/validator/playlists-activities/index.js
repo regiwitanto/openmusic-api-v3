e09c463810571472cc4254f3adf2ebf6362c8b12
Regi Witanto <witantoregi@gmail.com>
2024-08-01 10:26:36 +0700

    create get playlists validator

---
+++ src/validator/playlists-activities/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { getPlaylistsActivitiesPayloadSchema } = require('./schema');
+
+const PlaylistsActivitiesValidator = {
+  validateGetPlaylistsActivitiesPayload: (payload) => {
+    const validationResult =
+      getPlaylistsActivitiesPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = PlaylistsActivitiesValidator;

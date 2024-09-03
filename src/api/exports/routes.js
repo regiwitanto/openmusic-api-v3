ccf12407e7aa18d8eb674588abf4a918f1d84479
Regi Witanto <witantoregi@gmail.com>
2024-09-03 14:09:05 +0700

    fix export playlists endpoint name

--- src/api/exports/routes.js
+++ src/api/exports/routes.js
@@ -1,7 +1,7 @@
 const routes = (handler) => [
   {
     method: 'POST',
-    path: '/exports/playlists',
+    path: '/export/playlists',
     handler: (request, h) => handler.postExportPlaylistHandler(request, h),
     options: {
       auth: 'openmusic_jwt',
